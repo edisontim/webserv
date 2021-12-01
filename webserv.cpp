5e1724dd9c037f5e6154feb5b8cb52469713c112
tedison <tedison@student.s19.be>
2021-12-01 12:22:40 +0100

    Documentation update

--- webserv.cpp
+++ webserv.cpp
@@ -30,6 +30,17 @@ enum DT {CURRENT, LAST_MODIFIED};
 //_____________HTTP webserver logic:_____________
 //
 //1. parse configuration file if given, if not have a default path to look into. Base the parsing on the "server" part of Nginx's conf file
+//		see http://nginx.org/en/docs/http/ngx_http_core_module.html for documentation and response statuses on nginx directives
+//		- Chose the port of each "server"													listen 
+// 		- Chose the host of each "server"													listen 
+// 		- Setup of the server_name															server_name 
+// 		- The first server for a host:port will be the default server for this host:port.
+// 		(it will answer all the requests that don't belong to another server)
+// 		- Limiting the size of the body for the clients										client_max_body_size
+//		- Setup of default error pages														error_page
+//		- Setup of the routes with one or multiple rules (routes won't be using regexes)	
+//				--> see pdf
+//		- CGI execution for certain file extensions
 //2. from this configuration file: 
 //		a) create a bound socket file descriptor on the correct ip and port
 //		b) set the working directory to the correct one where files need to be fetched
@@ -195,9 +206,12 @@ std::string response(std::string full_path, std::string http_v, int status)
 
 int main()
 {
+	//vector to hold our different vectors of pfds, one for each server block of our conf file.
+	std::vector<std::vector<struct pollfd> > servers;
+
 	//vector that holds all our fds, either our bound socket or a remote connection
 	std::vector<struct pollfd> pfds;
-	
+
 	//socket fd
 	Internet_socket bound_sock;
 	if (bound_sock.get_socket_fd() < 0)
